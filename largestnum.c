11bf472ad83715ebb684e79f10eaf97b44e9420d
vignesh143 <35801011+vignesh143@users.noreply.github.com>
2018-02-01 16:15:37 +0530

    Create largestnum.c

---
+++ largestnum.c
@@ -0,0 +1,22 @@
+#include<stdio.>
+void main()
+{
+int a,b,c;#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter three numbers:");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b && a>c)
+{
+    printf("%d",a);
+}
+else if(b>a && b>c)
+{
+    printf("%d",b);
+}
+else if(c>a && c>b)
+{
+    printf("%d",c);
+}
+}
