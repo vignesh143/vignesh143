7b354980501d353073108f0fa50904489fde9033
vignesh143 <35801011+vignesh143@users.noreply.github.com>
2018-02-04 16:03:22 +0530

    Create supermarket.c

---
+++ supermarket.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    printf("enter the rackno:");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:printf("vegetables");
+        break;
+        case 2:printf("minerals");
+        break;
+        case 3:printf("clothes");
+        break;
+    }}
+    
