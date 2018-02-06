48771026766094c1b8bee6e85993dd8dc5d9f682
vignesh143 <35801011+vignesh143@users.noreply.github.com>
2018-02-06 16:16:35 +0530

    Create vowelorconsonant.c

---
+++ vowelorconsonant.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    char ch;
+    printf("enter the alphabet:");
+    scanf("%c",&ch);
+    switch(ch)
+     {
+         case'a':
+         printf("vowels");
+         break;
+         case'e':
+         printf("vowels");
+         break;
+         case'i':
+         printf("vowels");
+         break;
+         case'o':
+         printf("vowels");
+         break;
+         case'u':
+         printf("vowels");
+         break;
+         default:
+         printf("consonant");
+         break;
+         
+     }   }
+    
