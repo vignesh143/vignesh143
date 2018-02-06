188d16751f09741a390a65cf7eae26fadd657c67
vignesh143 <35801011+vignesh143@users.noreply.github.com>
2018-02-06 16:53:40 +0530

    Create arraynk.c

---
+++ arraynk.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int i,a,b,sum=0;
+    int array[10];
+    printf("enter the size of array");
+    scanf("%d",&a);
+    printf("how many value do you want to find");
+    scanf("%d",&b);
+    printf("enter the array");
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&array[i]);
+        }
+        for(i=0;i<b;i++)
+        {
+            sum+=array[i];
+        }
+        printf("%d",sum);
+        
+    
+}
