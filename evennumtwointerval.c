ab2569755c262126b8db45267cb6e86095c47ff8
vignesh143 <35801011+vignesh143@users.noreply.github.com>
2018-02-13 16:32:42 +0530

    Create evennumtwointerval.c

---
+++ evennumtwointerval.c
@@ -0,0 +1,27 @@
+#include<stdio.h> 
+
+void main(){
+
+           int a,b,rem,i;
+
+           printf("enter the first number:\n");
+
+           scanf("%d",&a);
+
+           printf("enter the final number :\n");
+
+           scanf("%d",&b);
+
+           printf("\n The Even numbers between %d and %d are ",a,b); 
+
+           for(i=a; i<=b; ++i){
+
+               rem = i % 2;
+
+               if(rem == 0)
+
+               printf("\n  %d",i);
+
+           }
+}
+    
