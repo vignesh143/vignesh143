f9dfbb946e2bf6c750653675f21e6946990e1fb7
vignesh143 <35801011+vignesh143@users.noreply.github.com>
2018-02-13 16:24:05 +0530

    Create armstrongtwointervals.c

---
+++ armstrongtwointervals.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main()
+{
+  int a, b, i, temp, num, rem;
+  printf("Enter two numbers(intervals): ");
+  scanf("%d %d", &a, &b);
+  printf("Armstrong numbers between %d an %d are: ",a,b);
+  for(i=a+1; i<b; ++i)
+  {
+      temp=i;
+      num=0;
+      while(temp!=0)
+      {
+          rem=(temp%10);
+          num+=rem*rem*rem;
+          temp/=10;
+      }
+      if(i==num)
+      {
+          printf("%d ",i);
+       }
+       }
+       }
+  
+  
