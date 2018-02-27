67766e61bc652ea6bd29c42bdd0c4db3b0cb1a94
vignesh143 <35801011+vignesh143@users.noreply.github.com>
2018-02-27 02:42:28 -0800

    Create sortedarray.c

---
+++ sortedarray.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void main()
+{
+    int a,i,j,b[a];
+    printf("enter the number of  intenger:");
+    scanf("%d",&a);
+    printf("enter the intenger:");
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    
+    for(i=0;i<a;i++)
+    {
+        for(j=i+1;j<a;j++)
+        {
+            if(b[i]>b[j])
+            {
+           a=b[i];
+           b[i]=b[j];
+           b[j]=a;
+           
+        }
+    }}
+    printf("\nSorted array elements:");
+    for(i=0;i<a;i++)
+    {
+    printf("\n%d",b[i]);
+    }
+    
+}
