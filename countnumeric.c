1e95aa0c62405f3d18ba88813b56e85a886b3449
vignesh143 <35801011+vignesh143@users.noreply.github.com>
2018-03-15 03:47:03 -0700

    Create countnumeric.c

---
+++ countnumeric.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char str[50],i,c;
+printf("enter the word");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+if((str[i]>='.')&&(str[i]<='9'))
+{
+c++;
+}
+}
+printf("%d",c-1);
+}
